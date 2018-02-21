ddea5d72c42542f8baf2a2d3a04df7f2dbc038f3
Kosmarjus <marjus@gmail.com>
2018-02-21 23:26:17 +0200

    almost finalized

--- script.js
+++ script.js
@@ -1,45 +1,48 @@
 let dataArray = [],
-    targetID = '',
-    checkArray = [];
+    targetID = '';
 
+//random ID generatorius
+function uuidv4() {
+    return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
+        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+    )
+}
+
+//f-ja irasu issaugojimui i localstorage
 function storeInput() {
     let inputName = document.getElementById('input').value;
     let inputProperty1 = document.getElementById('property1').value;
     let inputProperty2 = document.getElementById('property2').value;
 
-
-    if (localStorage.getItem('dataArray')) {
-        checkArray = JSON.parse(localStorage.getItem('dataArray'));
-    } else {
-        checkArray = [];
-    }
-
-    var result = dataArray.filter(function (obj) {
+    var result = dataArray.filter(function (obj) { //f-ja objekto atrinkimui pagal ID
         return obj.id == targetID;
     })[0];
 
 
-    if (typeof result == 'undefined' || typeof result.id == 'undefined') { //store new input
+    if (typeof result == 'undefined' || typeof result.id == 'undefined') { //jei nera irasu - kuriam naujus
         dataArray.push({
-            id: Date.now(),
+            id: uuidv4(),
             name: inputName,
             property1: inputProperty1,
             property2: inputProperty2
         });
-        localStorage.setItem('dataArray', JSON.stringify(dataArray));
-        $('#promptModal').modal('hide');
 
-    } else { //update current input
-        console.log('aktyus senas' + result.id);
+    } else { //jei yra irasas - atnaujinam seno iraso propercius
+
         result.name = inputName;
         result.property1 = inputProperty1;
         result.property2 = inputProperty2;
-        localStorage.setItem('dataArray', JSON.stringify(dataArray));
-        $('#promptModal').modal('hide');
+
+
     }
+
+    localStorage.setItem('dataArray', JSON.stringify(dataArray)); //stumiam i localstorage
+    $('#promptModal').modal('hide'); //ir paslepiap modalini langa 
 }
 
-function printInput() {
+
+//f-ja irasu spausdinimui i .main klase
+function printInput() { 
     let destination = document.querySelector('.main');
     let printOut = '';
     for (const entry of dataArray) {
@@ -57,19 +60,19 @@ function printInput() {
     destination.innerHTML = printOut;
 }
 
+
+//funkcija modalinio lango lauku isvalymui 
 function clearInputs() {
     document.getElementById('input').value = "";
     document.getElementById('property1').value = "";
     document.getElementById('property2').value = "";
-    document.getElementById('deleteButton').style.visibility = 'hidden';
+    document.getElementById('deleteButton').style.visibility = 'hidden'; //tuo paciu paslepiam delete mygtuka jei nekoreguojam seno iraso
 }
 
+// jei yra duomenu data arrayjuje = juos paparsinam ir atspausdinam per print f-ja
 function render() {
     if (localStorage.getItem('dataArray')) {
         dataArray = JSON.parse(localStorage.getItem('dataArray'));
-
-        // counter = +dataArray[dataArray.length-1].id;
-
         printInput();
     }
 }
@@ -85,47 +88,48 @@ function search() {
     // console.log(results);
 }
 
+//f-ja modalinio lango uzpildymui
 function populateModal() {
-    console.log('i work');
+
     dataArray = JSON.parse(localStorage.getItem('dataArray'));
 
-
-    var result = dataArray.filter(function (obj) {
-        return obj.id == targetID;
+    var result = dataArray.filter(function (obj) { //f-ja objekto atrinkimui pagal ID
+        return obj.id == targetID;  
     })[0];
-    console.log('amagad' + result.id);
 
     document.getElementById('input').value = result.name;
     document.getElementById('property1').value = result.property1;
     document.getElementById('property2').value = result.property2;
 
-    document.getElementById('deleteButton').style.visibility = 'visible';
+    document.getElementById('deleteButton').style.visibility = 'visible'; //redaguojant irasa - padarome mygtuka delete matoma
 }
 
+
+//funkcija targetID paemimui
 function getID(e) {
     targetID = e.target.id;
-    console.log(targetID);
 }
-
+//targetID trigeriai
 document.querySelector('.main').addEventListener('mouseover', getID, false);
 document.querySelector('.main').addEventListener('focus', getID, true);
 
+
+
+
+
+
+//trynimo mygtuko funkcija
 function deleteCard() {
 
     let confirmation = confirm('Ar tikrai norite ištrinti šią kortelę?');
-
     if (confirmation == true) {
-        console.log('taip');
-
-        var result = dataArray.filter(function (obj) {
+        var result = dataArray.filter(function (obj) { //f-ja objekto atrinkimui pagal ID
             return obj.id == targetID;
         })[0];
-        console.log('pzpzpzppz' + result.id)
-        dataArray.splice(dataArray.findIndex(p => p.id == result.id), 1);
-        localStorage.setItem('dataArray', JSON.stringify(dataArray));
-    }
-    console.log(dataArray);
-    $('#promptModal').modal('hide');
-    render();
 
+        dataArray.splice(dataArray.findIndex(p => p.id == result.id), 1); //trinam irasa surasdami jo pozicija arrajuje pagal jame esancio ID verte (haha!)
+        localStorage.setItem('dataArray', JSON.stringify(dataArray)); //irasom nauja arraju
+    }
+    $('#promptModal').modal('hide'); //slepiam modalini langa po mygtuko paspaudimo
+    render(); //atnaujinam ekrana
 }
